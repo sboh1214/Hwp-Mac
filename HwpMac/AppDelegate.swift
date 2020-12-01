21648d36d9c344310fbd6805da0b2e29f3923953
Seungbin Oh <sboh1214@gmail.com>
2020-12-01 23:03:31 +0900

    Initial Commit

---
+++ HwpMac/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  HwpMac
+//
+//  Created by Seungbin Oh on 2020/12/01.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HwpMac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HwpMac
+//
+//  Created by Seungbin Oh on 2020/12/01.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
