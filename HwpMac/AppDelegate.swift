58a396b7f4474d8f1ebe66386a51b303d0264486
Seungbin Oh <sboh1214@gmail.com>
2020-12-02 18:13:42 +0900

    add touch bar

--- HwpMac/AppDelegate.swift
+++ HwpMac/AppDelegate.swift
@@ -2,15 +2,15 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
 
+    }
+    
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+
     }
-
-
+    
+    
 }
 
--- HwpMac/MainSplitViewController.swift
+++ HwpMac/MainSplitViewController.swift
@@ -7,7 +7,7 @@ class MainSplitViewController: NSSplitViewController {
         splitView.dividerStyle = .thin
         splitView.autosaveName = NSSplitView.AutosaveName(splitViewResorationIdentifier)
         splitView.identifier = NSUserInterfaceItemIdentifier(rawValue: splitViewResorationIdentifier)
-    
+        
         let vcLeft = storyboard!.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("Sidebar View")) as! NSViewController
         let vcMiddle = storyboard!.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("Document View")) as! NSViewController
         let vcRight = storyboard!.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("Inspector View")) as! NSViewController
